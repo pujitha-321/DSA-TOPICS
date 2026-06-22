341cac9aedcfb279f5078ed4bd24592871f75f9f
Pujitha <pujithamayakunta16@gmail.com>
2026-06-22 20:21:40 +0530

    Add files via upload

---
+++ ascendingRecursion.java
@@ -0,0 +1,13 @@
+public class ascendingRecursion {
+    public static void main(String[] args){
+        numbers(1);
+    }
+    public static void numbers(int n){
+        if(n == 6){
+            return;
+        }
+        System.out.println(n);
+        numbers(n+1);
+    }
+    
+}
